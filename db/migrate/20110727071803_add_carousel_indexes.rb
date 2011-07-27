eaf2481a8c6eb8e4e1614aad0341d19c9fa46e6a
Tim Morgan <github@timothymorgan.info>
2011-07-27 00:29:57 -0700

    Add indexes to accounts/show carousel queries

---
+++ db/migrate/20110727071803_add_carousel_indexes.rb
@@ -0,0 +1,11 @@
+class AddCarouselIndexes < ActiveRecord::Migration
+  def change
+    change_table :people do |t|
+      t.index [ :user_id, :has_photo, :me, :hours ], name: 'people_user_photo_me_hours'
+    end
+
+    change_table :destinations do |t|
+      t.index [ :user_id, :has_photo ], name: 'dest_user_photo'
+    end
+  end
+end
