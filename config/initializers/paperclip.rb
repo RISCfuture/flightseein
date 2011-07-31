f6b9a09c9b8494b8ee727e89659a8cc19f2f9b98
Tim Morgan <github@timothymorgan.info>
2011-07-31 01:39:24 -0700

    Do not post-process invalid images

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,9 +1,20 @@
 # Update has_attached_file to also optionally set a column indicating whether
 # a file is attached.
+#
+# Do not process invalid thumbnails.
 
 class ActiveRecord::Base
   def self.has_attached_file_with_bool(name, options={})
     has_attached_file_without_bool name, options
+
+    if options[:styles] then
+      define_method :check_file_size do
+        valid?
+        errors[:"#{name}_file_size"].blank?
+      end
+      send :"before_#{name}_post_process", :check_file_size
+    end
+
     if column_names.include?("has_#{name}") then
       before_save { |obj| obj.send :"has_#{name}=", obj.send(name).original_filename.to_bool; true }
       scope :"with_#{name}", where(:"has_#{name}" => true)
