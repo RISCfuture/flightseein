a45b67d48c673dcc5963e00de0c663cd7ae9cc30
Tim Morgan <github@timothymorgan.info>
2011-08-17 01:06:05 -0700

    Better configured cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Flightseein::Application.config.session_store :cookie_store, key: '_flightseein_session', domain: ".#{Flightseein::Configuration.routing.host}"
+Flightseein::Application.config.session_store :cookie_store, domain: ".#{Flightseein::Configuration.routing.host}", expire_after: 2.weeks
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
