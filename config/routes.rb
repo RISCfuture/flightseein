6393f14c4ec93f047fc550a5d4f8632ccab8ef18
Tim Morgan <github@timothymorgan.info>
2013-07-23 23:21:27 -0700

    Try to get Sidekiq routes working

--- config/routes.rb
+++ config/routes.rb
@@ -7,30 +7,30 @@ Flightseein::Application.routes.draw do
   end
 
   constraints(subdomain: SubdomainRouter::Config.default_subdomain) do
-    resources :users, only: [ :new, :create ]
-    resource :session, only: [ :new, :create, :destroy ]
+    resources :users, only: [:new, :create]
+    resource :session, only: [:new, :create, :destroy]
     resources :photographs, only: :index
 
     root(to: 'users#new')
   end
 
   constraints(SubdomainRouter::Constraint) do
-    resource :account, only: [ :show, :edit, :update, :destroy ]
-    resources :airports, only: [ :index, :show ] do
+    resource :account, only: [:show, :edit, :update, :destroy]
+    resources :airports, only: [:index, :show] do
       resources :flights, only: :index
     end
-    resources :flights, only: [ :index, :show, :edit, :update ] do
-      resources :photographs, only: [ :index, :create ]
+    resources :flights, only: [:index, :show, :edit, :update] do
+      resources :photographs, only: [:index, :create]
     end
-    resources :imports, only: [ :new, :create, :show ]
-    resources :people, only: [ :index, :show ] do
+    resources :imports, only: [:new, :create, :show]
+    resources :people, only: [:index, :show] do
       resources :flights, only: :index
     end
 
     root(to: 'accounts#show')
-
-    require 'admin_constraint'
-    require 'sidekiq/web'
-    mount Sidekiq::Web => '/sidekiq', constraints: AdminConstraint.new
   end
+
+  require 'admin_constraint'
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq', constraints: AdminConstraint.new
 end
