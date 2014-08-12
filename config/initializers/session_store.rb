c839cafbfcae23882de354c1caaffe8880725fe0
Tim Morgan <github@timothymorgan.info>
2014-08-11 21:18:43 -0700

    Rails 4.1 tweaks

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,3 @@ Rails.application.config.session_store :cookie_store,
                                        domain:       ".#{SubdomainRouter::Config.domain}",
                                        expire_after: 2.weeks,
                                        key:          '_flightseein_session'
-
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
