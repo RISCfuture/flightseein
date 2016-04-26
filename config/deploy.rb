20b5f8f948c8876ac95f3f1b0cb6dbd1aa5a6d26
Tim Morgan <github@timothymorgan.info>
2016-04-26 13:44:14 -0700

    Ruby 2.3.1; Rails 4.2.6

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
-lock '3.4.0'
+lock '3.5.0'
 
 set :application, 'flightseein'
 set :repo_url, 'timothymorgan.git:flightseein.git'
 
 set :deploy_to, '/var/www/www.flightsee.in'
-set :rvm_ruby_version, "2.3.0@#{fetch :application}"
+set :rvm_ruby_version, "2.3.1@#{fetch :application}"
 
 set :linked_files, %w{config/secrets.yml config/environments/production/paperclip.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
